0a2b81c9486a5bc0db479dcd80ce90d8da2b231d
cuciocj <cuciocj@gmail.com>
2020-03-22 12:34:00 +1300

    rearrange order

--- DDL.sql
+++ DDL.sql
@@ -10,18 +10,17 @@ CREATE TABLE `addresses` (
 );
 
 
-DROP TABLE IF EXISTS `albums`;
+DROP TABLE IF EXISTS `musicians`;
 
-CREATE TABLE `albums` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `copyright_date` varchar(15) NOT NULL,
-  `producer_id` varchar(50) NOT NULL,
-  `audio_format` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `albums_FK_producer` (`producer_id`),
-  CONSTRAINT `albums_FK_producer` FOREIGN KEY (`producer_id`) REFERENCES `musicians` (`ssn`) ON UPDATE CASCADE
+CREATE TABLE `musicians` (
+  `ssn` varchar(50) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `address_id` int(11),
+  UNIQUE KEY `ssn` (`ssn`),
+  KEY `musicians_FK_address` (`address_id`),
+  CONSTRAINT `musicians_FK_address` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`)
 );
+
 
 DROP TABLE IF EXISTS `instruments`;
 
@@ -33,16 +32,35 @@ CREATE TABLE `instruments` (
   UNIQUE KEY `name` (`name`)
 );
 
-DROP TABLE IF EXISTS `musicians`;
 
-CREATE TABLE `musicians` (
-  `ssn` varchar(50) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `address_id` int(11) NOT NULL,
-  UNIQUE KEY `ssn` (`ssn`),
-  KEY `musicians_FK_address` (`address_id`),
-  CONSTRAINT `musicians_FK_address` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`) ON UPDATE CASCADE
+DROP TABLE IF EXISTS `albums`;
+
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `copyright_date` varchar(15) NOT NULL,
+  `producer_id` varchar(50) NOT NULL,
+  `audio_format` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `albums_FK_producer` (`producer_id`),
+  CONSTRAINT `albums_FK_producer` FOREIGN KEY (`producer_id`) REFERENCES `musicians` (`ssn`)
 );
+
+
+DROP TABLE IF EXISTS `songs`;
+
+CREATE TABLE `songs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `album_id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `author_id` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `songs_FK_album` (`album_id`),
+  KEY `songs_FK_musician` (`author_id`),
+  CONSTRAINT `songs_FK_album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`)
+  CONSTRAINT `songs_FK_musician` FOREIGN KEY (`author_id`) REFERENCES `musicians` (`ssn`)
+);
+
 
 DROP TABLE IF EXISTS `song_instruments`;
 
@@ -55,19 +73,5 @@ CREATE TABLE `song_instruments` (
   KEY `song_instruments_FK_musician` (`musician`),
   CONSTRAINT `song_instruments_FK_instrument` FOREIGN KEY (`instrument_id`) REFERENCES `instruments` (`id`),
   CONSTRAINT `song_instruments_FK_musician` FOREIGN KEY (`musician`) REFERENCES `musicians` (`ssn`),
-  CONSTRAINT `song_instruments_FK_song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS `songs`;
-
-CREATE TABLE `songs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `album_id` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `author_id` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `songs_FK_album` (`album_id`),
-  KEY `songs_FK_musician` (`author_id`),
-  CONSTRAINT `songs_FK_album` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `songs_FK_musician` FOREIGN KEY (`author_id`) REFERENCES `musicians` (`ssn`) ON UPDATE CASCADE
+  CONSTRAINT `song_instruments_FK_song` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`)
 );
